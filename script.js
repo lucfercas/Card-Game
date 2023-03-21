28e3a09a1e94098f81909009ebb5f07937b6c317
Lucía (Lucy) Fernandez <121562718+lucfercas@users.noreply.github.com>
2023-03-21 18:04:43 +0000

    added shuffle function, fixed wrong flip

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
 const cards = document.querySelectorAll(".card");
 
 let xFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if(!xFlippedCard) {
         xFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+  secondCard = this;
+
+  checkMatch();
   }
+
+function checkMatch() {
+    if (firstCard.dataset.img === secondCard.dataset.img) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards () {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards () {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1200)
+}
+
+function resetBoard() {
+    [xFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  (function shuffle() {
+    cards.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPos;
+    });
+  })();
   
   cards.forEach(card => card.addEventListener('click', flipCard));
